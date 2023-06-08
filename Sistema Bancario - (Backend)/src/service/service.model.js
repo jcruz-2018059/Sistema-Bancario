534153b570cba4fcd85c0c04d26fe13bca450d6e
jgomez-2018112 <jgomez-2018112@kinal.edu.gt>
2023-06-08 00:05:40 -0600

    Se agrego el modelo de Service/Productos

---
+++ Sistema Bancario - (Backend)/src/service/service.model.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+const serviceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    }
+},{
+    versionKey: false
+});
+
+module.exports = mongoose.model('Service', serviceSchema);
